096646dd121968d6efcc679a60f966f87c79950f
Nercone <nercone@nercone.dev>
2026-02-22 17:24:42 +0900

    --

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 sudo /usr/bin/systemctl stop nercone-webserver
 /usr/bin/git pull
-/home/nercone/.local/bin/uv tool uninstall nercone-webserver --no-cache || true
-/home/nercone/.local/bin/uv tool install . --upgrade --no-cache
+/root/.local/bin/uv tool uninstall nercone-webserver --no-cache || true
+/root/.local/bin/uv tool install . --upgrade --no-cache
 sudo /usr/bin/systemctl start nercone-webserver
